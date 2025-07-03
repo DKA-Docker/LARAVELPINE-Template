2563555effba64e023ce1e79032d89d7c84f3f29
Yovangga Anandhika <dka.tech.dev@gmail.com>
2025-07-04 05:54:03 +0800

    add compose.yml

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -3,8 +3,6 @@ import * as ReactDOM from 'react-dom/client'
 import "./../css/app.css"
 import {PageContainer} from "./layout/pageContainer";
 
-
-
 // Render ke elemen dengan id 'app'
 const rootElement = document.getElementById('root')
 if (rootElement) {
