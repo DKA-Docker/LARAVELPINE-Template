c0805d26d290941501e17a6f7432f1676816fe08
Yovangga Anandhika <dka.tech.dev@gmail.com>
2025-07-21 09:23:26 +0800

    add fixed session

--- resources/js/maxton/pages/settings/managements/accounts.js
+++ resources/js/maxton/pages/settings/managements/accounts.js
@@ -1,11 +1,10 @@
 // accounts.js
 import $ from 'jquery';
+import axios from "axios";
 import Datatables from 'datatables.net-dt';
 import "datatables.net-dt/css/dataTables.dataTables.min.css"
 
 $(function () {
     let table = new Datatables($('#example'));
-    table.row.add([
-        "test",'test','test'
-    ]).draw();
+
 });
