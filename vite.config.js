1cc71652b97b6ab872d25d483fde7fe8ac18fbd8
Yovangga Anandhika <dka.tech.dev@gmail.com>
2025-11-12 22:38:20 +0800

    add live wire, adn ignore compose.yml

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    const theme = env.THEME_NAME || 'laravel'
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-});
+    return {
+        plugins: [
+            laravel({
+                input: [
+                    `resources/theme/${theme}/css/app.css`,
+                    `resources/theme/${theme}/js/app.js`,
+                ],
+                refresh: true,
+            }),
+            tailwindcss(),
+        ],
+    }
+})
+
