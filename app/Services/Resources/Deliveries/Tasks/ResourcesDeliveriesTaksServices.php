128daf37e4944ae43a4e446286aafdead105b088
Yovangga Anandhika <dka.tech.dev@gmail.com>
2025-11-22 20:29:50 +0800

    Add Factory to Model Factory

--- app/Services/Resources/Deliveries/Tasks/ResourcesDeliveriesTaksServices.php
+++ app/Services/Resources/Deliveries/Tasks/ResourcesDeliveriesTaksServices.php
@@ -2,7 +2,14 @@
 
 namespace App\Services\Resources\Deliveries\Tasks;
 
+use App\Repositories\Apps\Deliveries\Tasks\TasksRepository;
+
 class ResourcesDeliveriesTaksServices
 {
 
+    protected TasksRepository $tasksRepository;
+
+    public function __construct(){
+        $this->tasksRepository = new TasksRepository();
+    }
 }
