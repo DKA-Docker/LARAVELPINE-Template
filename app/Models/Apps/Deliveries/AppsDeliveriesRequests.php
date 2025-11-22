f4e8a21b4b6d3c6867a03b96ed51a52d9ce13c42
Yovangga Anandhika <dka.tech.dev@gmail.com>
2025-11-22 20:23:24 +0800

    Add Factory to Model Factory

--- app/Models/Apps/Deliveries/AppsDeliveriesRequests.php
+++ app/Models/Apps/Deliveries/AppsDeliveriesRequests.php
@@ -2,15 +2,17 @@
 
 namespace App\Models\Apps\Deliveries;
 
+use Database\Factories\Apps\Deliveries\Requests\AppsDeliveriesRequestsFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AppsDeliveriesRequests extends Model {
 
+    /** @use HasFactory<AppsDeliveriesRequestsFactory> */
     use HasFactory;
 
     public $incrementing = false;
     protected $keyType = 'string';
 
-    
+
 }
---
+++ database/factories/Apps/Deliveries/Requests/AppsDeliveriesRequestsFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Factories\Apps\Deliveries\Requests;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AppsDeliveriesRequestsFactory extends Factory
+{
+
+    public function definition()
+    {
+
+    }
+}
