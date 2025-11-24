2eb1886a702d086eb2169db99c0f3427247037a2
Yovangga Anandhika <dka.tech.dev@gmail.com>
2025-11-24 15:32:41 +0800

    add soft Deleted

--- resources/theme/metronic/js/app.ts
+++ resources/theme/metronic/js/app.ts
@@ -2,6 +2,7 @@ import "./bootstrap";
 import "./core.bundle.js";
 import "./../vendors/ktui/ktui.min.js";
 import "./widgets/general.js";
-import "./pages/dashboards/trackings.ts"
+import "./pages/dashboards/trackings.ts";
+import "./pages/dashboards/deliveries/requests";
 import "./layouts/demo1.js";
 
---
+++ resources/theme/metronic/js/pages/dashboards/deliveries/requests.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import $ from "jquery";
+import URI from "urijs";
+
+$(window).on('load', function () {
+    /**
+     * GET Full URI Current URL Saat Ini
+     */
+    const FullUriCurrentURL = URI(window.location);
+    /** Memecah Slash (/) menjadi Array **/
+    const segs = FullUriCurrentURL.segment(); //
+    /** Tambah Api Segment "api". di index 0 aray **/
+    segs.unshift('api');
+    /** Set kembali Segment URL **/
+    FullUriCurrentURL.segment(segs);
+    /** Make A Requests with Axios **/
+    axios({
+        url: `${FullUriCurrentURL}`,
+        method: "GET",
+    }).then((res) => {
+        console.log(res.data);
+    })
+
+});
