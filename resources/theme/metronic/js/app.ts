103b56aa25d9a646187db38c79266232585fdbc3
Yovangga Anandhika <dka.tech.dev@gmail.com>
2025-11-25 16:59:16 +0800

    new Api KT-Datatables

--- resources/theme/metronic/js/app.ts
+++ resources/theme/metronic/js/app.ts
@@ -1,6 +1,6 @@
 import "./bootstrap";
 import "./core.bundle.js";
-import "./../vendors/ktui/ktui.min.js";
+//import "./../vendors/ktui/ktui.min.js";
 import "./widgets/general.js";
 import "./pages/dashboards/trackings.ts";
 import "./pages/dashboards/deliveries/requests";
