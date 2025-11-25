3d0a7954cf9321aa615e4ad1c72b12a160cbb3e8
Yovangga Anandhika <dka.tech.dev@gmail.com>
2025-11-25 12:26:49 +0800

    adding relation Factory

--- database/factories/Apps/Deliveries/Requests/Packages/AppsDeliveriesRequestsPackagesFactory.php
+++ database/factories/Apps/Deliveries/Requests/Packages/AppsDeliveriesRequestsPackagesFactory.php
@@ -2,13 +2,22 @@
 
 namespace Database\Factories\Apps\Deliveries\Requests\Packages;
 
+use App\Models\Apps\Deliveries\Requests\Packages\AppsDeliveriesRequestsPackagesUnits;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class AppsDeliveriesRequestsPackagesFactory extends Factory
 {
 
-    public function definition()
+    public function definition(): array
     {
-
+        $unit = AppsDeliveriesRequestsPackagesUnits::factory()->create();
+        return [
+            'id' => (string) Str::uuid(),
+            'name' => $this->faker->title(),
+            'unit' => $unit->id,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
     }
 }
--- database/factories/Apps/Deliveries/Requests/Packages/Units/AppsDeliveriesRequestsPackagesUnitsFactory.php
+++ database/factories/Apps/Deliveries/Requests/Packages/Units/AppsDeliveriesRequestsPackagesUnitsFactory.php
@@ -3,12 +3,19 @@
 namespace Database\Factories\Apps\Deliveries\Requests\Packages\Units;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class AppsDeliveriesRequestsPackagesUnitsFactory extends Factory
 {
 
-    public function definition()
+    public function definition(): array
     {
 
+        return [
+            'id' => (string) Str::uuid(),
+            'name' => $this->faker->randomElement(['unit', 'pcs', 'box']),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
     }
 }
