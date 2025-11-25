a8eed46bb937c8a0cbe93bd0260ba58d61dd8bd9
Yovangga Anandhika <dka.tech.dev@gmail.com>
2025-11-25 10:58:38 +0800

    rename and refactor data request

--- database/migrations/apps/deliveries/requests/packages/2025_11_25_023619_create_apps_deliveries_requests_packages_table.php
+++ database/migrations/apps/deliveries/requests/packages/2025_11_25_023619_create_apps_deliveries_requests_packages_table.php
@@ -16,7 +16,13 @@ return new class extends Migration
             /** ini adalah row yang berisi id account yang dibuat agar system tau siapa yang melakukan request Di dalam database */
             $table->foreignUuid('account')->comment('adalah akun yang melakukan melakukan Request')->constrained('accounts' )->onDelete('cascade');
             /** Table ini menjadi child dari table request. karna deliveries request bisa memiliki multiple packages */
-            $table->foreignUuid('request')->comment('adalah id parent request Many To One')->constrained('apps_deliveries_request')->onDelete('cascade');
+            $table->foreignUuid('request')->comment('adalah id parent request Many To One')->constrained('apps_deliveries_requests')->onDelete('cascade');
+            /** adalah nama packages yang direquest untuk dilakukan pengiriman di dalam task assigned */
+            $table->string('name')->comment("Adalah Nama Packages Yang Akan Di Request Untuk Di kirim");
+            /** is a QTY jumlah item yang akan di kirim Di dalam request */
+            $table->bigInteger('qty')->comment("adalah jumlah item yang di request yang akan di kirim di dalam pengiriman");
+            /**  table yang relevant ke table unit packages */
+            $table->foreignUuid('unit')->comment('adalah unit data yang dipakai untuk mengukur satuan')->constrained('apps_deliveries_requests_packages_units' )->onDelete('cascade');
             /** adalah function yang digunakan untuk melakukan soft deleted di dalam, database agar data tidak di hapus secara otomatis */
             $table->softDeletes()->comment('parameter soft deleted');
             /** data waktu yang digunakan untuk melakukan pembuatan data timestamp di dalam database  */
--- database/migrations/apps/deliveries/requests/packages/units/2025_11_25_023618_create_apps_deliveries_requests_packages_units_table.php
+++ database/migrations/apps/deliveries/requests/packages/units/2025_11_25_023618_create_apps_deliveries_requests_packages_units_table.php
@@ -11,11 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        /** Buat Sebuah Database untuk deliveries request */
-        Schema::create('apps_deliveries_requests', function (Blueprint $table) {
+        Schema::create('apps_deliveries_requests_packages_units', function (Blueprint $table) {
             $table->uuid('id')->primary();
             /** ini adalah row yang berisi id account yang dibuat agar system tau siapa yang melakukan request Di dalam database */
             $table->foreignUuid('account')->comment('adalah akun yang melakukan melakukan Request')->constrained('accounts' )->onDelete('cascade');
+            /** is name of unit packages config */
+            $table->string("name")->comment("adalah nama unit packages di dalam Unit Packages");
+            /** is description of unit packages config */
+            $table->string("description")->comment("adalah description unit packages di dalam Unit Packages");
             /** adalah function yang digunakan untuk melakukan soft deleted di dalam, database agar data tidak di hapus secara otomatis */
             $table->softDeletes()->comment('parameter soft deleted');
             /** data waktu yang digunakan untuk melakukan pembuatan data timestamp di dalam database  */
@@ -28,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('apps_deliveries_requests');
+        Schema::dropIfExists('apps_deliveries_requests_packages_units');
     }
 };
