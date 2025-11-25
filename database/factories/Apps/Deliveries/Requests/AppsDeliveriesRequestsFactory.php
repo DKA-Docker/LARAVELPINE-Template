a45ce66074769e7621a97633c37388f025052b2e
Yovangga Anandhika <dka.tech.dev@gmail.com>
2025-11-25 12:33:50 +0800

    adding relation Factory

--- database/factories/Apps/Deliveries/Requests/AppsDeliveriesRequestsFactory.php
+++ database/factories/Apps/Deliveries/Requests/AppsDeliveriesRequestsFactory.php
@@ -2,13 +2,20 @@
 
 namespace Database\Factories\Apps\Deliveries\Requests;
 
+use App\Models\Base\Accounts\Accounts;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class AppsDeliveriesRequestsFactory extends Factory
 {
 
-    public function definition()
+    public function definition(): array
     {
-
+        $account = Accounts::inRandomOrder()->first();
+        return [
+            'id' => (string) Str::uuid(),
+            'account' => $account->id,
+            'name' => $this->faker->name()
+        ];
     }
 }
