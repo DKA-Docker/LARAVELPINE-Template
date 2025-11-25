ec2dfef234dedf3dde96f5c2f8c49a24a23a477e
Yovangga Anandhika <dka.tech.dev@gmail.com>
2025-11-25 12:21:21 +0800

    add unit Factory

---
+++ app/Models/Apps/Deliveries/Requests/Packages/Unit/AppsDeliveriesRequestsPackagesUnits.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models\Apps\Deliveries\Requests\Packages;
+
+use App\Models\Base\Accounts\Accounts;
+use Database\Factories\Apps\Deliveries\Requests\Packages\Units\AppsDeliveriesRequestsPackagesUnitsFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class AppsDeliveriesRequestsPackagesUnits extends Model
+{
+    /** @use HasFactory<AppsDeliveriesRequestsPackagesUnitsFactory> */
+    use HasFactory, SoftDeletes;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    /** Kolom yang bisa diisi */
+    protected $fillable = [
+        'id',
+        'account',
+        'name',
+        'description',
+        'created_at',
+        'updated_at',
+    ];
+    /** Cast tipe data */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    /**
+     * Relation Data Account Untuk Table Ini Di dalam database
+     * @return HasOne
+     */
+    public function account(): HasOne
+    {
+        return $this->hasOne(Accounts::class, 'account')->withDefault();
+    }
+}
---
+++ database/factories/Apps/Deliveries/Requests/Packages/Units/AppsDeliveriesRequestsPackagesUnitsFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Factories\Apps\Deliveries\Requests\Packages\Units;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AppsDeliveriesRequestsPackagesUnitsFactory extends Factory
+{
+
+    public function definition()
+    {
+
+    }
+}
