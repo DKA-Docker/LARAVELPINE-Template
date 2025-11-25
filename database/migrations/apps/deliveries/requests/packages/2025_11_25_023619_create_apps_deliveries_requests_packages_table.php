8fbe9ad850568e8492e5ec2c9ff3568f89948e72
Yovangga Anandhika <dka.tech.dev@gmail.com>
2025-11-25 10:43:00 +0800

    rename and refactor data request

--- database/migrations/apps/deliveries/requests/packages/2025_11_25_023619_create_apps_deliveries_requests_packages_table.php
+++ database/migrations/apps/deliveries/requests/packages/2025_11_25_023619_create_apps_deliveries_requests_packages_table.php
@@ -12,7 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('apps_deliveries_requests_packages', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            /** ini adalah row yang berisi id account yang dibuat agar system tau siapa yang melakukan request Di dalam database */
+            $table->foreignUuid('account')->comment('adalah akun yang melakukan melakukan Request')->constrained('accounts' )->onDelete('cascade');
+            /** Table ini menjadi child dari table request. karna deliveries request bisa memiliki multiple packages */
+            $table->foreignUuid('request')->comment('adalah id parent request Many To One')->constrained('apps_deliveries_request')->onDelete('cascade');
+            /** adalah function yang digunakan untuk melakukan soft deleted di dalam, database agar data tidak di hapus secara otomatis */
+            $table->softDeletes()->comment('parameter soft deleted');
+            /** data waktu yang digunakan untuk melakukan pembuatan data timestamp di dalam database  */
             $table->timestamps();
         });
     }
