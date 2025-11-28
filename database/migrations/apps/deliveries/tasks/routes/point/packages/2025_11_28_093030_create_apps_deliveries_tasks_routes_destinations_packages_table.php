2dd828baac50ba51ba423dcbf251f0195ca3b15c
ryancyber <aryan358701@gmail.com>
2025-11-28 17:55:28 +0800

    tambah migration app deliveries tasks destinations packages

---
+++ database/migrations/apps/deliveries/tasks/routes/point/packages/2025_11_28_093030_create_apps_deliveries_tasks_routes_destinations_packages_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apps_deliveries_tasks_routes_destinations_packages', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            /** ini adalah row yang berisi id account yang dibuat agar system tau siapa yang melakukan request Di dalam database */
+            $table->foreignUuid('account')->comment('adalah akun yang melakukan melakukan Request')->constrained('accounts' )->onDelete('cascade');
+            /** adalah nama packages yang direquest untuk dilakukan pengiriman di dalam task assigned */
+            $table->string('name')->comment("Adalah Nama Packages Yang Akan Di Request Untuk Di kirim");
+            /** is a QTY jumlah item yang akan di kirim Di dalam request */
+            $table->bigInteger('qty')->comment("adalah jumlah item yang di request yang akan di kirim di dalam pengiriman");
+            /**  table yang relevant ke table unit packages */
+            $table->foreignUuid('unit')->comment('adalah unit data yang dipakai untuk mengukur satuan')->constrained('apps_deliveries_requests_packages_units' )->onDelete('cascade');
+            /**  table yang relevant ke table unit packages */
+            $table->foreignUuid('destination')->comment('adalah destination data yang dipakai untuk mengambil destinasi')->constrained('apps_deliveries_tasks_routes_destinations' )->onDelete('cascade');
+            /** Buat Note Untuk Request packages Ini */
+            $table->string("note")->comment('adalah catatan yang ditinggalkan oleh Requested kepada logistik')->nullable();
+            /** Adalah Dimensi Panjang (width) */
+            $table->integer('dimension_width')->nullable()->comment("adalah dimension width")->default(0);
+            /** Adalah Dimensi Lebar (height) */
+            $table->integer('dimension_height')->nullable()->comment("adalah dimension height")->default(0);
+            /** adalah dimensi Tinggi (weight) */
+            $table->integer('dimension_weight')->nullable()->comment("adalah dimension weight")->default(0);
+            /** adalah satuan berat dalam Ons */
+            $table->integer('heavy')->comment("adalah jumlah Berat Barang Dalam ons")->default(0);
+            /** Menandai packages Ini Sebagai Barang Mudah Pecah */
+            $table->boolean('is_fragile')->comment("Menandai packages ini sebagai Barang Mudah Pecah")->default(false);
+            /** adalah function yang digunakan untuk melakukan soft deleted di dalam, database agar data tidak di hapus secara otomatis */
+            $table->softDeletes()->comment('parameter soft deleted');
+            /** data waktu yang digunakan untuk melakukan pembuatan data timestamp di dalam database  */
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apps_deliveries_tasks_routes_destinations_packages');
+    }
+};
