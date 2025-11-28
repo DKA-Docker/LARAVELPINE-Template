1901a4bcc6e8e8ebe8b2c7bf51aa7a7a73744bf1
Yovangga Anandhika <dka.tech.dev@gmail.com>
2025-11-28 14:56:54 +0800

    FIX SEEDER. ndk ada S nya pak

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use App\Http\Controllers\V1\Frontend\Index as FrontendIndex;
 use Illuminate\Support\Facades\Route;
 
 /** Sumber Halaman Frontend Utama */
-Route::resource('/', FrontendIndex::class)->parameters(['' => 'id']);
+Route::resource('/', FrontendIndex::class);
 /** Grouping Ke Group Auth Di Dalam Web Routes*/
 Route::prefix('auth')->name('auth.')->group(function () {
     /** Name Route dashboards.apps */
-    Route::resource('/', FrontendAuth::class)->parameters(['' => 'id']);
+    Route::resource('/', FrontendAuth::class);
 });
 /** name Route dashboards */
 Route::middleware(['auth:web'])->prefix('dashboards')->name('dashboards.')->group(function () {
