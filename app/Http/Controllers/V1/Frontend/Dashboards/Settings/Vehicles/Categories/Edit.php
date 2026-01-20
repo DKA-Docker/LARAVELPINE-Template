b300558ad90fb091efd2d0983f9709ba91ff9efc
Yovangga Anandhika <dka.tech.dev@gmail.com>
2026-01-20 10:43:20 +0800

    refactor: replace FQCN with use statements in vehicle categories Edit.php

--- app/Http/Controllers/V1/Frontend/Dashboards/Settings/Vehicles/Categories/Edit.php
+++ app/Http/Controllers/V1/Frontend/Dashboards/Settings/Vehicles/Categories/Edit.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\V1\Frontend\Dashboards\Settings\Vehicles\Categori
 use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 use App\Models\Data\Vehicles\DataVehicleCategories;
+use App\Services\Resources\Data\Vehicles\DataVehicleCategoriesServices;
 
 class Edit extends Controller
 {
     public function index(Request $request, $id)
     {
-        $service = new \App\Services\Resources\Data\Vehicles\DataVehicleCategoriesServices();
+        $service = new DataVehicleCategoriesServices();
         $category = $service->Find($id);
         
         return view('dashboards.settings.vehicles.categories.edit', [
