077d219e7cf4a340f7beb4117d269bbe2113337e
Yovangga Anandhika <dka.tech.dev@gmail.com>
2026-01-20 10:44:00 +0800

    refactor: clean up docblock in UserOnline.php

--- app/Events/Chats/UserOnline.php
+++ app/Events/Chats/UserOnline.php
@@ -36,7 +36,7 @@ class UserOnline implements ShouldBroadcastNow
     /**
      * Get the channels the event should broadcast on.
      *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
+     * @return array<int, Channel>
      */
     public function broadcastOn(): array
     {
