a95fc51850b75dab6cc13b9c6b320655469756b7
Yovangga Anandhika <dka.tech.dev@gmail.com>
2026-01-20 10:43:24 +0800

    refactor: replace FQCN with use statements in vehicle Edit.php

--- app/Http/Controllers/V1/Frontend/Dashboards/Settings/Vehicles/Edit.php
+++ app/Http/Controllers/V1/Frontend/Dashboards/Settings/Vehicles/Edit.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\V1\Frontend\Dashboards\Settings\Vehicles;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Route;
 use App\Models\Data\Vehicles\DataVehicles;
+use App\Services\Resources\Data\Vehicles\DataVehiclesServices;
 use Illuminate\Http\Request;
 
 class Edit extends Controller
@@ -12,7 +13,7 @@ class Edit extends Controller
     public function index(Request $request, $id)
     {
         // Use Service to find vehicle
-        $service = new \App\Services\Resources\Data\Vehicles\DataVehiclesServices();
+        $service = new DataVehiclesServices();
         $vehicle = $service->Find($id);
         
         return view('dashboards.settings.vehicles.edit', [
