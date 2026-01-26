acd0549f133c3496fd1e2c56442a1ec072b68339
Yovangga Anandhika <dka.tech.dev@gmail.com>
2026-01-26 10:35:22 +0800

    templating ...

--- resources/theme/metronic/js/app.ts
+++ resources/theme/metronic/js/app.ts
@@ -2,17 +2,4 @@ import "./bootstrap";
 import "./core.bundle.js";
 //import "./../vendors/ktui/ktui.min.js";
 //import "./widgets/general.js";
-import "./pages/dashboards/trackings.ts";
-import "./pages/dashboards/dashboard.ts";
-import "./components/session-monitor.ts";
-import "./pages/chat/chat.ts"; // Chat Reverb Logic\
-import "./pages/dashboards/deliveries/requests/create.ts";
-import "./pages/dashboards/deliveries/tasks/index";
-import "./pages/dashboards/deliveries/tasks/create";
-import "./pages/dashboards/deliveries/tasks/detail.ts";
-import "./pages/dashboards/deliveries/tasks/edit";
-import "./pages/dashboards/deliveries/tasks/show";
-import "./pages/dashboards/deliveries/tasks/sessions/view.ts";
-import "./pages/managements/accounts/index";
-import "./pages/dashboards/deliveries/requests/detail.ts";
 //import "./layouts/demo1.js";
